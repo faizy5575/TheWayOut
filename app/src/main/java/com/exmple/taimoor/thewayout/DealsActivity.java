3befb8fa09a86acc80bedb98845ce2b87d6ac359
faizy5575 <35286439+faizy5575@users.noreply.github.com>
2018-01-31 15:11:04 +0500

    Updated deals activity and added new deals activiy

--- app/src/main/java/com/exmple/taimoor/thewayout/DealsActivity.java
+++ app/src/main/java/com/exmple/taimoor/thewayout/DealsActivity.java
@@ -1,13 +1,29 @@
 package com.exmple.taimoor.thewayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class DealsActivity extends AppCompatActivity {
+    Button mAddNewDeal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deals);
+        mAddNewDeal = (Button) findViewById(R.id.addNewDealbutton);
+        mAddNewDeal.setOnClickListener(onAddDeal);
     }
+
+    public View.OnClickListener onAddDeal = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(DealsActivity.this, NewDeal.class);
+            startActivity(intent);
+        }
+    };
 }
+
