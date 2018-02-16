8c7efe573e7c39c07ccad2953c93f4c232c90bdb
faizy5575 <35286439+faizy5575@users.noreply.github.com>
2018-02-16 16:38:31 +0500

    sessions

--- app/src/main/java/com/exmple/taimoor/thewayout/homeactivities/Constants.java
+++ app/src/main/java/com/exmple/taimoor/thewayout/homeactivities/Constants.java
@@ -3,7 +3,7 @@ package com.exmple.taimoor.thewayout.homeactivities;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://192.168.53.99:80/TheWayOut/";
+    private static final String ROOT_URL = "http://192.168.53.99:80/theWayOut/v1/";
 
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
     public static final String URL_LOGIN = ROOT_URL+"loginUser.php";
